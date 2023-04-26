908b0f104182fa3a5a3e3483764a6cf3a8e0a43a
Pasca George-Alexandru <alexpasca2000@gmail.com>
2023-04-26 23:21:16 +0300

    Add login, logout and register methods to AuthService

--- src/app/auth/AuthService.ts
+++ src/app/auth/AuthService.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
 import {BehaviorSubject} from "rxjs";
+import {first, tap} from "rxjs/operators";
 import {JwtHelperService} from "@auth0/angular-jwt";
+import {IUser} from "../user/user.model";
 
 @Injectable({
   providedIn: "root"
 })
 export class AuthService {
-  resourceUrl = "";
+  resourceUrl = "localhost:8000";
   isUserLoggedIn$ = new BehaviorSubject<boolean>(false)
 
   constructor(
@@ -16,4 +18,29 @@ export class AuthService {
     protected router: Router,
     protected jwtHelper: JwtHelperService
   ) { }
+
+  login(user: IUser) {
+    return this.http.post(this.resourceUrl + "/login", user, { observe: "response" })
+      .pipe(
+        first(),
+        tap((res: any) => {
+          this.isUserLoggedIn$.next(true);
+          localStorage.setItem("token", res.body.token);
+          this.router.navigate(["/"]);
+        })
+      );
+  }
+
+  logout() {
+    localStorage.clear();
+    window.location.href = '/';
+  }
+
+  register(user: IUser) {
+    return this.http.post(this.resourceUrl + "/register", user, { observe: "response" });
+  }
+
+  isLogged(): boolean {
+    return !!localStorage.getItem("token");
+  }
 }
