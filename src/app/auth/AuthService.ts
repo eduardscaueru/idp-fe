17c2aa224050c7763693860c9f6374667408ea96
eduardscaueru <edi_scaueru@yahoo.com>
2023-05-02 19:57:20 +0300

    Fixed auth endpoints

--- src/app/auth/AuthService.ts
+++ src/app/auth/AuthService.ts
@@ -19,7 +19,7 @@ export class AuthService {
   ) { }
 
   login(user: IUser) {
-    return this.http.post(this.resourceUrl + "/api/login", user, { observe: "response" })
+    return this.http.post(this.resourceUrl + "/api/auth/login", user, { observe: "response" })
       .pipe(
         first(),
         tap((res: any) => {
@@ -36,7 +36,8 @@ export class AuthService {
   }
 
   register(user: IUser) {
-    return this.http.post(this.resourceUrl + "/api/register", user, { observe: "response" });
+    console.log("ggggg")
+    return this.http.post(this.resourceUrl + "/api/auth/register", user, { observe: "response" });
   }
 
   isLogged(): boolean {
