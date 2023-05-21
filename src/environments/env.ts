f8ff101b6f38c49976f42ebfd73a4cf6102901a2
eduardscaueru <edi_scaueru@yahoo.com>
2023-05-21 13:15:18 +0300

    Fixed kong endpoint

--- src/environments/env.ts
+++ src/environments/env.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  AUTH_URL: 'http://proiect_kong:8000/auth',
+  AUTH_URL: 'http://kong:8000/auth',
   GROUP_URL: 'http://kong:8000/be/group',
   POST_URL: 'http://kong:8000/be/post',
   USER_URL: 'http://kong:8000/be/user'
